4c43fea249a1594048e962785d2e9e1c63d2d962
waqar-haider-hol <waqar.haider@holcim.com>
2026-02-16 00:18:27 +0530

    initial commit

---
+++ streamline.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import requests
+import json
+
+# Configure streamlit page
+st.set_page_config(page_title="AI Chat with Amazon bedrock", 
+                   page_icon="🤖", 
+                   layout="centered")
