3f271a7c96782f16a146988d2baf34063d76f576
waqar-haider-hol <waqar.haider@holcim.com>
2026-02-16 11:37:22 +0530

    required changes

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import requests
+import json
+
+# Configure streamlit page
+st.title=("AI Chat with Amazon bedrock") 
+            
--- streamline.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import requests
-import json
-
-# Configure streamlit page
-st.set_page_config(page_title="AI Chat with Amazon bedrock", 
-                   page_icon="🤖", 
-                   layout="centered")
